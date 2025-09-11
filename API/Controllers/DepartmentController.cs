ca65fe14781ea25b52eeae20e1df4f85a3376fc1
ASSEM MLG <20812022100248@eng.zu.edu.eg>
2025-09-11 07:39:32 +0300

    add department endpoints

--- API/Controllers/DepartmentController.cs
+++ API/Controllers/DepartmentController.cs
@@ -15,33 +15,55 @@ public class DepartmentController(IDepartmentService departmentService) : Contro
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> GetDepartments()
     {
+        var result =  await _departmentService.GetAllDepartmentsAsync();
+        if(!result.Success)
+            return NotFound();
         
+        return Ok(result.Data);
     }
 
     [HttpGet("{id}")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> GetDepartmentById(int id)
     {
+        var result = await _departmentService.GetDepartmentByIdAsync(id);
+        if(!result.Success)
+            return NotFound();
         
+        return Ok(result.Data);
     }   
     
     [HttpPost]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> CreateDepartment(DepartmentRequest department)
     {
+        var result = await _departmentService.CreateDepartmentAsync(department);
+        if(!result.Success)
+            return BadRequest();
         
+        return Ok(result.Data);
     }
 
     [HttpPost("update")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> UpdateDepartment(int id, DepartmentRequest department)
     {
+        var result = await _departmentService.UpdateDepartmentAsync(id, department);
         
+        if(!result.Success)
+            return BadRequest();
+        
+        return Ok();
     }
     [HttpDelete("{id}")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> DeleteDepartment(int id)
     {
+        var result = await _departmentService.DeleteDepartmentAsync(id);
         
+        if(!result.Success)
+            return BadRequest();
+        
+        return Ok();
     }
 }
