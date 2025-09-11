f3e2a51ee4c5518dfa0de5eea75cc0a5a8961ace
ASSEM MLG <20812022100248@eng.zu.edu.eg>
2025-09-11 06:54:49 +0300

    add skeleton endpoints

--- API/Controllers/DepartmentController.cs
+++ API/Controllers/DepartmentController.cs
@@ -1,6 +1,46 @@
-namespace StudentManagerAPI.API.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagerAPI.API.Services;
+using StudentManagerAPI.Entities;
 
-public class DepartmentController
+namespace StudentManagerAPI.API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class DepartmentController(IDepartmentService departmentService) : ControllerBase
 {
+    private readonly IDepartmentService _departmentService =  departmentService;
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetDepartments()
+    {
+        
+    }
+
+    [HttpGet("{id}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetDepartmentById(int id)
+    {
+        
+    }   
     
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateDepartment(DepartmentRequest department)
+    {
+        
+    }
+
+    [HttpPost("update")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> UpdateDepartment(int id, DepartmentRequest department)
+    {
+        
+    }
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteDepartment(int id)
+    {
+        
+    }
 }
