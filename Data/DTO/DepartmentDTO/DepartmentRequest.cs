0b8460d9ec39d684854e1fc23b572815ff671f0a
ASSEM MLG <20812022100248@eng.zu.edu.eg>
2025-09-11 07:35:38 +0300

    Department Validation

--- Data/DTO/DepartmentDTO/DepartmentRequest.cs
+++ Data/DTO/DepartmentDTO/DepartmentRequest.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StudentManagerAPI.API.Seeding.DTO.DepartmentDTO;
 
 public class DepartmentRequest
 {
+    [Required]
+    [Length(5, 50)]
     public string Name { get; set; }
+    [Required]
+    [Length(5, 200)]
     public string Description { get; set; }
 
 }
